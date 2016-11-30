6ee758a6d2a4ed2d4916a7661c464b86cc5e57fb
Rolleander <roll2@web.de>
2016-11-30 18:26:09 +0100

    added project

---
+++ LibGDX/android/src/com/broll/poklmon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.broll.poklmon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.broll.poklmon.PoklmonGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PoklmonGame(), config);
+	}
+}
